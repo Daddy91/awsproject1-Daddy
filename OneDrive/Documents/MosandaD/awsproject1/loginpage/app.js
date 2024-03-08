852810002a7261359fc4d818f5d4d15a2d7462d5
Daddy91 <daddymosanda@live.fr>
2024-03-08 12:06:32 +0100

    modified the h1

---
+++ OneDrive/Documents/MosandaD/awsproject1/loginpage/app.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://sfu6x3sy1c.execute-api.us-east-1.amazonaws.com/test/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setMessage('OK');
+      } else {
+        setMessage(data.message); // Assuming lambda returns a message on failure
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setMessage('Error logging in. Please try again.', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Welcome to my login</h2>
+      <h1>Testing ECS!</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default LoginPage;
