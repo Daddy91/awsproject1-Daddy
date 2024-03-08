9280f681d8d1736ff70faaacfa6b4a49b07355be
Daddy91 <daddymosanda@live.fr>
2024-03-08 12:42:32 +0100

    update

--- OneDrive/Documents/MosandaD/awsproject1/loginpage/servers.js
+++ OneDrive/Documents/MosandaD/awsproject1/loginpage/servers.js
@@ -6,12 +6,12 @@ const app = express();
 app.use('/health', (req, res) => {
     //res.status(200).send('OK'); // Respond with a status code of 200 and 'OK'
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    const html = '<html><body><h1>Hello World App</h1></body></html>';
+    const html = '<html><body><h1>Hello Daddy</h1></body></html>';
     res.end(html);
 });
 
 // Start the server on a specific port (e.g., 8080)
 const port = 8080;
 app.listen(port, () => {
-    console.log(`Hello world app started & listening on port ${port}!`);
+    console.log(`Hello Daddy started & listening on port ${port}!`);
 });
